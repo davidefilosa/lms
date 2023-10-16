9e37636d233ae5aaaf16e574109e28fc89ada67c
Davide Filosa <davideclaypool@gmail.com!>
2023-10-16 08:13:59 +0200

    authentication with clerk

--- app/(dashboard)/(routes)/page.tsx
+++ app/(dashboard)/(routes)/page.tsx
@@ -1,10 +1,11 @@
 import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
     <>
       <div className="text-green-500 text-xl">This a protected page</div>
-      <Button>Click me</Button>
+      <UserButton afterSignOutUrl="/" />
     </>
   );
 }
