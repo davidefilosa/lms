fea7625b7723708f4ebea3fd7ae88311cf4cfe3f
Davide Filosa <davideclaypool@gmail.com!>
2023-10-20 11:06:14 +0200

    created coursed data table

--- app/(dashboard)/(routes)/teacher/courses/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/page.tsx
@@ -1,13 +1,34 @@
-import { Button } from "@/components/ui/button";
-import Link from "next/link";
-import React from "react";
+import { auth } from "@clerk/nextjs";
+import { columns } from "./_components/columns";
+import { DataTable } from "./_components/data-table";
+import { redirect } from "next/navigation";
+import { prismadb } from "@/lib/prismadb";
 
-const CousesPage = () => {
+async function getData(): Promise<any[]> {
+  // Fetch data from your API here.
+  return [
+    {
+      id: "728ed52f",
+      amount: 100,
+      status: "pending",
+      email: "m@example.com",
+    },
+    // ...
+  ];
+}
+
+const CousesPage = async () => {
+  const { userId } = auth();
+
+  if (!userId) {
+    return redirect("/");
+  }
+
+  const courses = await prismadb.course.findMany({ where: { userId } });
+
   return (
     <div className="p-6">
-      <Link href="/teacher/create">
-        <Button>New Course</Button>
-      </Link>
+      <DataTable columns={columns} data={courses} />
     </div>
   );
 };
